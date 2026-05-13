c093b2c91b5d6d388749fee49e24d0b23c8420ae
Isaque-Codes <isaque.137.silva@gmail.com>
2026-05-13 11:13:21 -0300

    ✨ feat: Tela de login finalizada

--- web/src/pages/login/index.tsx
+++ web/src/pages/login/index.tsx
@@ -3,7 +3,29 @@ import styles from "./login.module.css"
 const Login = () => {
     return (
         <>
-
+            <main className="layout_guide" id={styles.main}>
+                <section id={styles.esquerda}>
+                    <figure id={styles.fig1}>
+                        <img src="../imgs/mulher-login.png" alt="Imagem de personagem feminina com temática cyberpunk à esquerda do formulário de login" />
+                    </figure>
+                </section>
+                <section id={styles.direita}>
+                    <figure id={styles.fig2}>
+                        <img src="../imgs/logo-header.png" alt="Imagem da logo do Royal Games acima dos campos de login" />
+                    </figure>
+                    <form id={styles.form} action="">
+                        <div>
+                            <label htmlFor="">E-mail</label>
+                            <input className={styles.input} type="email" />
+                        </div>
+                        <div id="ajuste">
+                            <label htmlFor="">Senha</label>
+                            <input className={styles.input} type="password" />
+                        </div>
+                        <button>Entrar</button>
+                    </form>
+                </section>
+            </main>
         </>
     )
 }
