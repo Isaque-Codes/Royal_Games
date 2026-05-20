e4c4d619eb395a8bc8787d95a4877c3d6bdf1fc7
Isaque-Codes <isaque.137.silva@gmail.com>
2026-05-20 10:58:07 -0300

    ✨ feat: Prototipo de tela com qualidade duvidosa para testes de consumo da API na tela home

--- web/src/pages/api/authService.ts
+++ web/src/pages/api/authService.ts
@@ -4,15 +4,15 @@ import secureLocalStorage from "react-secure-storage";
 export async function login(Email: string, Senha: string) {
     try {
         const response = await api.post("Autenticacao/login", { Email, Senha });
-         console.log("Funcionou!");
-         console.log(response.data.token);
+        //  console.log("Funcionou!");
+        //  console.log(response.data.token);
         const token = response.data.token;
 
         localStorage.setItem("nomeToken", token);
         secureLocalStorage.setItem("Token", token);
 
     } catch (error: any) {
-        console.error("Erro:", error.response?.data || error.message);
+        // console.error("Erro:", error.response?.data || error.message);
         throw new Error("Email ou senha inválidos");
     }
 }
--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -1,9 +1,31 @@
-import styles from "./home.module.css"
+import Footer from "@/components/footer/footer";
+import Header from "@/components/header/header";
+import styles from "./home.module.css";
+import ListaJogo from "@/components/lista-jogo/lista-jogo";
 
 const Home = () => {
     return (
         <>
-
+            <Header />
+            <main id={styles.home}>
+                <div className={styles.banner}>
+                    <div className={styles.texto_banner}>
+                        <p className={styles.titulo}>Conheça nossos jogos!</p>
+                        <p className={styles.texto}>
+                            Navegue por títulos de todas as gerações, descubra plataformas,
+                            gêneros e detalhes completos antes de escolher sua próxima aventura.
+                            Seu próximo jogo favorito começa aqui.
+                        </p>
+                    </div>
+                    <img src="../imgs/cyberpunk.png" alt="" id={styles.imagem_banner} />
+                </div>
+                <div id={styles.catalogo}>
+                    <p className={styles.titulo}>Catálogo de Jogos</p>
+                    <hr id={styles.linha}></hr>
+                    <ListaJogo />
+                </div>
+            </main>
+            <Footer />
         </>
     )
 }
